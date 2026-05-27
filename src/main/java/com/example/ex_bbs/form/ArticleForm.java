d89a718e945387a9cc1f149069037177e28f1b56
natsukintk8 <oya8n.xwof10@gmail.com>
2026-05-27 16:14:49 +0900

    記事投稿時に使用するフォームを作成しました

---
+++ src/main/java/com/example/ex_bbs/form/ArticleForm.java
@@ -0,0 +1,35 @@
+package com.example.ex_bbs.form;
+
+/**
+ * 記事投稿時に使用するフォームです.
+ */
+public class ArticleForm {
+    /**　投稿者名　*/
+    private String name;
+    /**　投稿内容　*/
+    private String content;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "name='" + name + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
