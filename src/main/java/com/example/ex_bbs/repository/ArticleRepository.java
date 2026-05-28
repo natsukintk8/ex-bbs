6638ad24e864e1dce0cdbdd6266c83620e62ad5e
natsukintk8 <oya8n.xwof10@gmail.com>
2026-05-28 10:14:24 +0900

    記事を投稿するメソッドを追加しました

--- src/main/java/com/example/ex_bbs/repository/ArticleRepository.java
+++ src/main/java/com/example/ex_bbs/repository/ArticleRepository.java
@@ -4,7 +4,10 @@ import com.example.ex_bbs.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -35,5 +38,23 @@ public class ArticleRepository {
                   id DESC
                 """;
         return template.query(sql,ARTICLES_ROW_MAPPER);
+    }
+
+    /**
+     * 記事を投稿する.
+     *
+     * @param article 記事
+     */
+    public void insert(Article article){
+        String sql = """
+                 INSERT INTO
+                  articles
+                  (name, content)
+                 VALUES
+                  (:name, :content)
+                """;
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(article);
+        template.query(sql,param,ARTICLES_ROW_MAPPER);
     }
 }
